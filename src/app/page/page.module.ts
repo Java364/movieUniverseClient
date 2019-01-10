957c432e8836666e056bd40e0934aac1dbdcc174
Vitalii Lototskyi <loto9308@gmail.com>
2019-01-10 13:10:05 +0200

    main page changes

--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { AdminModule } from './admin/admin.module';
 import { PageComponent } from './page.component';
 import { UserModule } from './user/user.module';
-import { NavbarModule } from '../components/navbar/navbar.module';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [PageComponent
