e5946c5f802315354e2a88d842ce1f5dd25094be
Maksym-Paselsky <43058251+Maksym-Paselsky@users.noreply.github.com>
2019-01-10 16:56:16 +0200

    ad movie

---
+++ src/app/components/movie/movie.ts
@@ -0,0 +1,21 @@
+export class Movie {
+
+    name: string;
+    year: number;
+    id: number;
+    description :string;
+    duration: number;
+    ageLimitation: string;
+    self: string;
+    genres: string;
+    poster: string;
+    gallery: string;
+    stars: string;
+    roles: string;
+    trailers: string;
+    countries: string;
+    comments: string;
+    movieMark: string;
+    constructor() {
+    }
+}
