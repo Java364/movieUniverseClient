616ca11c16e5834a981e7861b8250f8a38b94237
ArtemFrostM <frostifrost00@gmail.com>
2019-01-11 15:18:52 +0200

    added logic how to share id between star-component and link-component

--- src/app/components/star/star.ts
+++ src/app/components/star/star.ts
@@ -7,6 +7,12 @@ export class Star {
   biography: string;
   growth: number;
   cityOfBirth: string;
+  gallery: string;
+  avatar: string;
+  roles: string;
+  links: string;
+  countries: string;
+  professions: string;
   constructor() {
   }
 }
--- src/app/page/admin/admin.module.ts
+++ src/app/page/admin/admin.module.ts
@@ -8,7 +8,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ComponentsModule } from 'src/app/components/components.module';
 
 const appRoutes: Routes = [
-  {path: 'star', component: StarPageComponent},
+  {path: 'admin/star', component: StarPageComponent},
   {path: 'country', component: CountryPageComponent}
 ];
 
