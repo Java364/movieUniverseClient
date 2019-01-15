bff25e3ce68e57d3352ac8c2f99dc923924273ab
Solomiia <yaremkosolomiya@gmail.com>
2019-01-15 19:00:19 +0200

    change login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { PageModule } from './page/page.module';
 import {
   MatFormFieldModule, MatSnackBarModule
 } from '@angular/material';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
