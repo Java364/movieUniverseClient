57d02c459e8c95e3e5b0268d7f69544354f85b40
Solomiia <yaremkosolomiya@gmail.com>
2019-01-15 11:34:53 +0200

    auth pac..

--- src/app/page/auth/user.module.ts
+++ src/app/page/auth/user.module.ts
@@ -1,23 +1,24 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {MainPageComponent} from "./main/main-page/main-page.component";
 import {RouterModule, Routes} from "@angular/router";
 import {BrowserModule} from "@angular/platform-browser";
 import {ComponentsModule} from "src/app/components/components.module";
 import {NavbarComponent} from "src/app/components/navbar/navbar.component";
-
-import {MoviePageComponent} from "./movie-page/movie-page.component";
-
+import {SignupPageComponent} from "./signup-page/signup-page.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {ForgotPageComponent} from "./forgot/forgot-page.component";
 
 
 const appRoutes: Routes = [
-  { path: 'main', component: MainPageComponent },
 
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'login/forgot', component: ForgotPageComponent }
 ];
 
 @NgModule({
 
-  declarations: [MainPageComponent, MoviePageComponent],
+  declarations: [SignupPageComponent, LoginPageComponent, ForgotPageComponent],
 
   imports: [
     RouterModule.forRoot(
@@ -29,10 +30,10 @@ const appRoutes: Routes = [
     ComponentsModule,
   ],
   exports: [
-    MainPageComponent,
+
     NavbarComponent,
     RouterModule,
-    MoviePageComponent
+
   ]
 })
-export class UserModule { }
+export class AuthModule { }
