b44f713bd3ef2670610f5d7899c4bb005207b5eb
Marichka <lunishinm@gmail.com>
2019-01-16 22:08:12 +0200

    avatar add to star list

--- src/app/components/avatar/avatar.service.ts
+++ src/app/components/avatar/avatar.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpService } from 'src/app/core/http.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AvatarService {
 
-  constructor(private httpService: HttpService) { }
+    constructor(private httpService: HttpService) { }
 
-  public showStarsAvatar(link: string, callback: Function) {
-    this.httpService.get<AvatarService>(link).subscribe(
-        (success) => {
-            callback(success);
-        }
-    );
-}
+    public showStarsAvatar(link: string, callback: Function) {
+        this.httpService.get<AvatarService>(link).subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
+
+    public getAvatar(link:string, callback: Function) {
+        this.httpService.get(link).subscribe(
+            (success) => {
+                callback(success);
+            }
+        );
+    }
 }
